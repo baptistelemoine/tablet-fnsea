a30aec3329e2a19f43c1d20862ef71e5242d70d3
baptistelemoine <baptiste.lemoine@gmail.com>
2013-09-25 14:11:54 +0200

    compress gzip

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var path = require('path');
 var app = module.exports = express();
 
 app.configure(function () {
-    app.use(express.logger('dev'));
+    // app.use(express.logger('dev'));
+    app.use(express.compress());
     app.use(express.bodyParser());
     app.use(express.static(path.join(__dirname, 'public')));
 });
