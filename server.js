1441e61f559ff3ab3213054dd43f1e85a60c8f8e
baptistelemoine <baptiste.lemoine@gmail.com>
2013-09-25 14:13:36 +0200

    enable gzip compression

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var app = module.exports = express();
 
 app.configure(function () {
+	app.use(express.compress());
     app.use(express.static(path.join(__dirname, 'public')));
 });
 
